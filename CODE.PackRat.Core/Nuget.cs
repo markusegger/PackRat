693776d85c8649b02b567087695211e402bb5c39
Markus Egger <megger@eps-software.com>
2016-09-20 02:19:26 -0500

    no message

---
+++ CODE.PackRat.Core/Nuget.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NuGet;
+
+namespace CODE.PackRat.Core
+{
+    public static class NuGet
+    {
+        private static string PackageSource { get; set; } = "https://packages.nuget.org/api/v2";
+        private static string PackageDestination { get; set; } = @"c:\PackRatTest\";
+
+        private static IPackageRepository GetRepository()
+        {
+            return PackageRepositoryFactory.Default.CreateRepository(PackageSource);
+        }
+
+
+        public static List<IPackage> SearchRepository(string searchTerm)
+        {
+            return GetRepository().Search(searchTerm, true).ToList();
+        }
+
+        public static bool GetPackage(string packageId, string packageVersion, string destinationPath = null)
+        {
+            if (string.IsNullOrEmpty(destinationPath)) destinationPath = PackageDestination;
+            var packageManager = new PackageManager(GetRepository(), destinationPath);
+            packageManager.InstallPackage(packageId, SemanticVersion.Parse(packageVersion));
+            return true;
+        }
+    }
+}
---
+++ CODE.PackRat.TestBench.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace CODE.PackRat.TestBench.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SearchPackages(object sender, RoutedEventArgs e)
+        {
+            var packages = Core.NuGet.SearchRepository(searchTerm.Text.Trim());
+
+            var sb = new StringBuilder();
+            foreach (var package in packages)
+                sb.AppendLine(package.ToString());
+
+            results.Text = sb.ToString();
+
+            var firstPackage = packages.FirstOrDefault();
+            if (firstPackage != null)
+                Core.NuGet.GetPackage(firstPackage.Id, firstPackage.Version.ToString());
+        }
+    }
+}
